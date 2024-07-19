209039f25589e1919a79df83828a6375d46a9096
8edij1-9w20ksn201-1o02 <121732697+ossamarayan22@users.noreply.github.com>
2024-07-19 15:30:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+const product = [
+    {
+        id: 0,
+        image: 'image/gg-1.jpg',
+        title: 'iphone 14 plus',
+        price: 800,
+    },
+    {
+        id: 1,
+        image: 'image/gg-1.jpg',
+        title: 'iphone 14 plus',
+        price: 800,
+    },
+    {
+        id: 2,
+        image: 'image/gg-1.jpg',
+        title: 'iphone 14 plus',
+        price: 800,
+    },
+    {
+        id: 3,
+        image: 'image/gg-1.jpg',
+        title: 'iphone 14 plus',
+        price: 800,
+    },
+    {
+        id: 4,
+        image: 'image/gg-1.jpg',
+        title: 'iphone 14 plus',
+        price: 800,
+    },
+    {
+        id: 5,
+        image: 'image/gg-1.jpg',
+        title: 'iphone 14 plus',
+        price: 800,
+    },
+];
+
+const categories = [...new Set(product.map((item) => { return item }))]
+
+document.getElementById('searchBar').addEventListener('keyup', (e) => {
+    const searchData = e.target.value.toLowerCase();
+    const filteredData = categories.filter((item) => {
+        return (
+            item.title.toLowerCase().includes(searchData)
+        )
+    })
+    displayItem(filteredData)
+});
+
+const displayItem = (items) => {
+    document.getElementById('root').innerHTML = items.map((item) => {
+        var { image, title, price } = item;
+        return (
+            `<div class='box'>
+                <div class='img-box'>
+                    <img class='images' src=${image}></img>
+                </div> 
+                <div class='bottom'>
+                    <p>${title}</p>
+                    <h2>${price}.00 MAD</h2>
+                <button id="sent">Unlock Now</button>
+                </div>
+            </div>`
+        )
+    }).join('')
+};
+displayItem(categories);
